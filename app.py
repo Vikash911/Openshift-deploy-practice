5dab1ae7226145895fdf6f5057904c2ae824b7ff
Vikash Bhade <82812943+Vikash911@users.noreply.github.com>
2025-07-23 12:15:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 # app.py
+import cv2
+import numpy as np
 
 def main():
-    print("hi")
+    # Create a black image
+    image = np.zeros((100, 100, 3), dtype=np.uint8)
+    cv2.circle(image, (50, 50), 40, (255, 0, 0), -1)
+    cv2.imwrite("output.png", image)
+    print("Image created and saved as output.png")
 
 if __name__ == "__main__":
     main()
